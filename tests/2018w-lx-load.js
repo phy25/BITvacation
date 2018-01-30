2f4c3bde237921603bcd53934f1968557f0714ca
Phy <git@phy25.com>
2018-01-30 20:29:08 +0800

    add tests and travis.yml

---
+++ tests/2018w-lx-load.js
@@ -0,0 +1,33 @@
+casper.test.begin('2018w-lx loads without error', function testLoad(test) {
+	var error = false;
+	casper.options.onError = function(c, m, b){
+		error = m;
+	};
+	/*
+	casper.on('error', function(msg, backtrace) {
+	    error = msg;
+	});
+	*/
+	casper.start('./dist/2018w-lx.html');
+	casper.waitFor(function check() {
+	    return this.evaluate(function() {
+	    	var n = document.getElementById('page-name');
+	    	var l = document.querySelector('#nav-jump a');
+	        return l || (n && n.innerHTML.indexOf('失败') >= 0);
+	    });
+	}, function then(){
+		var pageName = this.fetchText('#page-name');
+		test.assertNot(pageName.indexOf('失败') >= 0, '#page-name says load is not failed; if this fails please check manually');
+		test.pass("If not failed previously, it has loaded now");
+	}, function timeout(){
+		test.fail("#nav-jump is not populated (timeout)");
+		// this.echo(this.getHTML());
+	}, 5000);
+
+	casper.run(function(){
+		var pageTitle = this.getTitle();
+		test.assertMatch(pageTitle, /假/, 'title contains related symbol');
+		test.assertNot(error, 'Page load has no error');
+		test.done();
+	});
+});
